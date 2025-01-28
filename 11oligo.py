28c759378ecb1ae78495e784392b4fc07ad69e5d
Justin_Phan185 <jlpphan@ucdavis.edu>
2025-01-28 15:27:11 -0800

    updated

--- 11oligo.py
+++ 11oligo.py
@@ -1,10 +1,12 @@
-#return Oligopeptide melting temp given number of AAs
+#return Oligopeptide length and melting temp given a number of AAs
 
 def oligo(A,T,G,C):
 	length = (A+T+G+C)
+	print("Your oligopeptide length is", length)
 	if length <= 13:
 		tm = (A+T)*2 + (G+C)*2
 	else: tm = 64.9 +41*(G+C - 16.4)/(A+T+G+C)
 	return tm
-print(oligo(5,7,3,4))
-
+print("Oligo1 melting temp:", oligo(5,7,3,4))
+print("Oligo3 melting temp:", oligo(1,1,1,1))
+print("Oligo3 melting temp:", oligo(24,24,17,17))
